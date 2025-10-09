43d1a57b2053e906d5b6fab53b40e43299fe4e71
Ubuntu <ubuntu@ip-172-31-22-115.ec2.internal>
2025-10-09 17:27:13 +0000

    add production IP address

--- mediproject/mediproject/settings.py
+++ mediproject/mediproject/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e9%y(6=lmfw)=7!og8_$kz^h8#zft7n!1&%rbed9mwiy7nwv_^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '54.226.4.183']
+
 
 
 # Application definition
