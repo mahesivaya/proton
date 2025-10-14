ea7e48ad2951e85a315f49c137d91cf8cb75ec07
mahesivaya <167259736+mahesivaya@users.noreply.github.com>
2025-10-13 22:53:35 -0400

    Reception app

---
+++ proton/reception/views.py
@@ -0,0 +1,24 @@
+from audioop import add
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+from accounts.decorators import role_required
+from accounts.models import Patient
+from django.contrib import messages
+
+# Create your views here.
+
+
+@role_required(allowed_roles=['reception'])
+@login_required
+def reception_dashboard(request):
+    if request.method == 'POST':
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+        age = request.POST.get("age")
+        email = request.POST.get("email")
+        phone_number = request.POST.get("phone_number")
+        address = request.POST.get("address")
+        patient = Patient.objects.create(first_name=first_name,last_name=last_name, age=age, email=email,phone_number=phone_number,address=address)
+        messages.success(request, "Patient registered successfully")
+        patient.save()
+    return render(request, 'reception/reception_dashboard.html')
